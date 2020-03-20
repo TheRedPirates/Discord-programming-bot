e6fced90848fefccf65306c271b17ad23ceb4f86
Ido Hadar <ido.hadar03@gmail.com>
2020-03-20 23:58:30 +0200

    Updated jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     final char botPrefix = '*';
 
     public static void main(String[] args) throws LoginException {
-        JDA client = JDABuilder.createDefault("NTgxODIwNzk0NzU0MTcwOTAw.XnU3WQ.d_IChqM9bJ780IUR5Rrd7pMS33I")
+        JDA client = JDABuilder.createDefault("NTgxODIwNzk0NzU0MTcwOTAw.XnU4Kw.DWbIriHMC-DAL1DSTeUD3YZPK70")
                 .setEventManager(new AnnotatedEventManager())
                 .addEventListeners(new Main())
                 .build();
