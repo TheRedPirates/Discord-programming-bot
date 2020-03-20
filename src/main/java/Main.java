43897703acd4b73be402a90735c62ca3e8277717
Ido Hadar <ido.hadar03@gmail.com>
2020-03-20 19:41:13 +0200

    Finished minimal bot

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.AnnotatedEventManager;
+import net.dv8tion.jda.api.hooks.SubscribeEvent;
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    final char botPrefix = '*';
+
+    public static void main(String[] args) throws LoginException {
+        JDABuilder.createDefault("NTgxODIwNzk0NzU0MTcwOTAw.XnTkog.x4QvV3jW4CCKdiftN5ePNeKiKlo")
+                .setEventManager(new AnnotatedEventManager())
+                .addEventListeners(new Main())
+                .build();
+    }
+
+    @SubscribeEvent
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (!event.getAuthor().isBot() && event.getMessage().getContentDisplay().startsWith(botPrefix + "hi")) {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Hello!").queue();
+        }
+    }
+}
